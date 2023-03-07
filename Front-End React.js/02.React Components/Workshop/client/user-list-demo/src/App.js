a420b8f773275c4a2abc92076b4b26bc25076aaa
Stefan Kantardzhiev <stefan.ivailov@gmail.com>
2023-03-07 16:23:08 +0100

    Workshop

--- Front-End React.js/02.React Components/Workshop/client/user-list-demo/src/App.js
+++ Front-End React.js/02.React Components/Workshop/client/user-list-demo/src/App.js
@@ -1,15 +1,17 @@
 import { Header } from "./components/Header";
 // import { Fragment } from 'react'
 import { Footer } from "./components/Footer";
-
+import { Search } from "./components/Search"
+import './App.css'
 function App() {
   return (
     <>
       <Header />
       <main className="main">
+        <section class="card users-container">
 
-
-
+          <Search />
+        </section>
       </main>
       <Footer />
     </>
