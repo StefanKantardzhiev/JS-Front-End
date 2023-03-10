bc17887c61df554f9bc47342b6f5359303ab67a9
Stefan Kantardzhiev <stefan.ivailov@gmail.com>
2023-03-10 16:41:02 +0100

    Workshop

--- Front-End React.js/02.React Components/Workshop/client/user-list-demo/src/App.js
+++ Front-End React.js/02.React Components/Workshop/client/user-list-demo/src/App.js
@@ -1,22 +1,38 @@
+import { useEffect, useState } from 'react';
+
+import * as userService from './services/userService'
 import { Header } from "./components/Header";
-// import { Fragment } from 'react'
+import { Fragment } from 'react'
 import { Footer } from "./components/Footer";
 import { Search } from "./components/Search"
-import './App.css'
+import '../src/App.css'
 import { UserList } from "./components/UserList";
+
+
+
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    userService.getAll()
+      .then(setUsers)
+      .catch(err => {
+        console.log('Error' + err)
+      })
+  })
   return (
-    <>
+    <Fragment>
       <Header />
       <main className="main">
-        <section class="card users-container">
+        <section className="card users-container">
 
           <Search />
-          <UserList />
+          <UserList users={users} />
         </section>
       </main>
       <Footer />
-    </>
+    </Fragment>
   );
 }
 
