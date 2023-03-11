252175d76bb1b556c3d5ad4e721eba9fd05cc2d6
Stefan Kantardzhiev <stefan.ivailov@gmail.com>
2023-03-11 13:57:11 +0100

    Workshop Pt.1 finish

--- Front-End React.js/02.React Components/Workshop/client/user-list-demo/src/App.js
+++ Front-End React.js/02.React Components/Workshop/client/user-list-demo/src/App.js
@@ -1,39 +1,41 @@
 import { useEffect, useState } from 'react';
 
-import * as userService from './services/userService'
+import * as userService from './services/userService';
+
 import { Header } from "./components/Header";
-import { Fragment } from 'react'
 import { Footer } from "./components/Footer";
-import { Search } from "./components/Search"
-import '../src/App.css'
+import { Search } from "./components/Search";
+import './App.css';
 import { UserList } from "./components/UserList";
 
-
-
 function App() {
+    const [users, setUsers] = useState([]);
 
-  const [users, setUsers] = useState([]);
+    useEffect(() => {
+        userService.getAll()
+            .then(setUsers)
+            .catch(err => {
+                console.log('Error' + err);
+            });
+    }, []);
 
-  useEffect(() => {
-    userService.getAll()
-      .then(setUsers)
-      .catch(err => {
-        console.log('Error' + err)
-      })
-  })
-  return (
-    <Fragment>
-      <Header />
-      <main className="main">
-        <section className="card users-container">
+    return (
+        <>
+            <Header />
 
-          <Search />
-          <UserList users={users} />
-        </section>
-      </main>
-      <Footer />
-    </Fragment>
-  );
+            <main className="main">
+                <section className="card users-container">
+                    <Search />
+
+                    <UserList users={users} />
+
+                    <button class="btn-add btn">Add new user</button>
+                </section>
+            </main>
+
+            <Footer />
+        </>
+    );
 }
 
 export default App;
--- Front-End React.js/02.React Components/Workshop/client/user-list-demo/src/components/UserDetails.js
+++ Front-End React.js/02.React Components/Workshop/client/user-list-demo/src/components/UserDetails.js
@@ -1,21 +1,25 @@
-export const UserDetails = (
-    {
-        _id,
-        firstName,
-        lastName,
-        email,
-        phoneNumber,
-        address
-    }
-) => {
-    return(
-        <div className="overlay">
+
+
+export const UserDetails = ({
+    _id,
+    firstName,
+    lastName,
+    email,
+    imageUrl,
+    createdAt,
+    updatedAt,
+    phoneNumber,
+    address,
+    onClose,
+}) => {
+    return (
+        <div className="overlay" >
             <div className="backdrop"></div>
             <div className="modal">
                 <div className="detail-container">
                     <header className="headers">
                         <h2>User Detail</h2>
-                        <button className="btn close">
+                        <button className="btn close" onClick={onClose}>
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark"
                                 className="svg-inline--fa fa-xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                                 <path fill="currentColor"
@@ -26,28 +30,27 @@ export const UserDetails = (
                     </header>
                     <div className="content">
                         <div className="image-container">
-                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt=""
-                                className="image" />
+                            <img src={imageUrl} alt={`${firstName} ${lastName}`} className="image" />
                         </div>
                         <div className="user-details">
                             <p>User Id: <strong>{_id}</strong></p>
                             <p>
                                 Full Name:
-                                <strong> {firstName}{lastName} </strong>
+                                <strong> {`${firstName} ${lastName}`} </strong>
                             </p>
                             <p>Email: <strong>{email}</strong></p>
                             <p>Phone Number: <strong>{phoneNumber}</strong></p>
                             <p>
                                 Address:
-                                <strong> {address.country}{address.city}{address.street}{address.streetNumber}</strong>
+                                <strong> {`${address.country}, ${address.city}, ${address.street} ${address.streetNumber}`}</strong>
                             </p>
 
-                            <p>Created on: <strong>Wednesday, June 28, 2022</strong></p>
-                            <p>Modified on: <strong>Thursday, June 29, 2022</strong></p>
+                            <p>Created on: <strong>{Date(createdAt).slice(0,10)}</strong></p>
+                            <p>Modified on: <strong>{Date(updatedAt).slice(0,10)}</strong></p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        </div >
+    );
+};
