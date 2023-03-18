f57770eb0729d8911ff0cae4124daa7da110d6f0
Stefan Kantardzhiev <stefan.ivailov@gmail.com>
2023-03-18 17:07:14 +0100

    Routing Workshop

---
+++ Front-End React.js/04.Routing/Workshop/car-dealer/src/services/gameService.js
@@ -0,0 +1,21 @@
+import * as request from "./requester"
+
+
+const baseUrl = 'http://127.0.0.1:3030/jsonstore/games'
+
+export const getAll = async () => {
+    const result = await request.get(baseUrl)
+    const games = Object.values(result)
+    // console.log(games)
+    return games
+}
+
+export const create = async (gameData) => {
+    const result = await request.post(baseUrl, gameData)
+    return result
+}
+
+export const getOne = async (gameId) => {
+    const result = await request.get(`${baseUrl}/${gameId}`)
+    return result
+}
